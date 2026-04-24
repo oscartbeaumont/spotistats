4bc0b0c9b61cf3d0b438a5d56b41cec08602ce92
Oscar Beaumont <oscar@otbeaumont.me>
2026-04-24 09:00:01 +0800

    TailwindCSS

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,10 @@
 import { Title } from "@solidjs/meta";
-import Counter from "~/components/Counter";
 
 export default function Home() {
   return (
     <main>
       <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
+      <h1 class="bg-red-500">Hello world!</h1>
       <p>
         Visit{" "}
         <a href="https://start.solidjs.com" target="_blank">
